44b82598794a4e67cdf87b569d05411db7e90445
DontsovD <triggonayt@yandex.ru>
2022-09-15 14:07:56 +0300

    Add gitignore

--- lectures1_HelloConsole/Program.cs
+++ lectures1_HelloConsole/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Enter your name");
 string username = Console.ReadLine();
-Console.Write("Hello,");
-Console.Write(username);
+Console.WriteLine("Привет,");
+Console.WriteLine(username);
